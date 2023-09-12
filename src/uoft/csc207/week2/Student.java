7ce488d06a79cfb4a3c9183dc34f28e27f2e92be
Anietta Weckauff <aniettaweckauff@Aniettas-MacBook-Air-4.local>
2023-09-12 14:20:07 -0400

    updates

--- src/uoft/csc207/week2/Student.java
+++ src/uoft/csc207/week2/Student.java
@@ -1,14 +1,16 @@
 package uoft.csc207.week2;
 
 public class Student extends Person{
+    private final String studentID;
 
     /**
-     * Initialize this Person named name with UTORid utorid.
      *
-     * @param name   the person's name (family name last)
-     * @param utorid the person's UTORid
+     * @param name
+     * @param utorid
+     * @param studentID
      */
     Student(String[] name, String utorid, String studentID) {
         super(name, utorid);
+        this.studentID = studentID;
     }
 }
